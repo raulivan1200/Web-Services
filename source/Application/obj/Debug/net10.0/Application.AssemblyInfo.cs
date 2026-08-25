f266b95f5e822af6a9f9517356fd17c434f8e2a5
Raul <66335304+raulivan1200@users.noreply.github.com>
2026-08-25 12:48:39 -0600

    clean program cs

--- source/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ source/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14ff7a447d9133b80b168b8fbd67e5b97915ffa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd34f411b48ec549fbe0b0e9204571c6361f093")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
